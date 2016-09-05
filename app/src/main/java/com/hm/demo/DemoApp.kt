22ad244e9a9a122aeb6ba5d1f4d1d603ca4e3e79
bxcx <bxcx@163.com>
2016-09-05 21:10:45 +0800

    no message

---
+++ app/src/main/java/com/hm/demo/DemoApp.kt
@@ -0,0 +1,16 @@
+package com.hm.demo
+
+import com.hm.library.app.HMApp
+
+/**
+ * DemoApp
+ *
+ * himi on 2016-09-05 20:55
+ * version V1.0
+ */
+class DemoApp : HMApp() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
