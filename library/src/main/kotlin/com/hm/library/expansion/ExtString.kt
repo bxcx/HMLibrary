194bfc723e46c0c3831dda9a7f1b9b2e6cbc73f8
himi <bxcx@163.com>
2016-09-24 09:20:33 +0800

    change Material to https://github.com/rey5137/material

--- library/src/main/kotlin/com/hm/library/expansion/ExtString.kt
+++ library/src/main/kotlin/com/hm/library/expansion/ExtString.kt
@@ -1,11 +1,20 @@
 package com.hm.library.expansion
 
+import java.util.regex.Pattern
+
 /**
  * ExtString
  *
  * himi on 2016-03-25 14:47
  * version V1.0
  */
-var String.toZN : CharSequence
-    get() = this.replace(",","，")
-    set(str) {}
+val String.toZN: CharSequence
+    get() = this.replace(",", "，")
+
+val String.isEmail: Boolean
+    get() {
+        val check = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$"
+        val regex = Pattern.compile(check)
+        val matcher = regex.matcher(this)
+        return matcher.matches()
+    }
